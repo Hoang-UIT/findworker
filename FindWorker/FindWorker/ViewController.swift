c9593f1fe0627cc16f809aec3de0d8c441d9f093
hoangtp-mobile <hoangtp@rakumo.vn>
2020-08-20 14:11:55 +0700

    Create new project.

---
+++ FindWorker/FindWorker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindWorker
+//
+//  Created by hoangtp on 8/20/20.
+//  Copyright © 2020 Rakumo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
