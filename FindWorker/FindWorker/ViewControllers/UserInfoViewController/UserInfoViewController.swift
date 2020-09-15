ab76e15dbe4580c097113e48170d72d0ed7ca0d7
hoangtp-mobile <hoangtp@rakumo.vn>
2020-09-15 18:01:38 +0700

    Update 15/09/2020

--- FindWorker/FindWorker/ViewControllers/UserInfoViewController/UserInfoViewController.swift
+++ FindWorker/FindWorker/ViewControllers/UserInfoViewController/UserInfoViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class UserInfoViewController: BaseViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
+    @IBOutlet weak var addressTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         titleViewController = "Thông Tin Người Dùng"
+        
+        emailTextField.isEnabled = false
+        emailTextField.delegate = self
+        
+        phoneNumberTextField.isEnabled = false
+        phoneNumberTextField.delegate = self
+        
+        addressTextView.isEditable = false
+        addressTextView.delegate = self
+        addressTextView.translatesAutoresizingMaskIntoConstraints = true
+        addressTextView.sizeToFit()
+        addressTextView.isScrollEnabled = false
     }
     
     @IBAction func updatePasswordBtnAction (sender: UIButton) {
@@ -21,7 +39,41 @@ class UserInfoViewController: BaseViewController {
         }
     }
     
+    @IBAction func emailBtnAction(_ sender: UIButton) {
+        emailTextField.isEnabled = true
+        emailTextField.becomeFirstResponder()
+    }
+    
+    @IBAction func phoneBtnAction(_ sender: UIButton) {
+        phoneNumberTextField.isEnabled = true
+        phoneNumberTextField.becomeFirstResponder()
+    }
+    
+    @IBAction func addressBtnAction(_ sender: UIButton) {
+        addressTextView.isEditable = true
+        addressTextView.becomeFirstResponder()
+    }
+    
     @IBAction func logoutBtnAction (sender: UIButton) {
         navigationController?.dismiss(animated: true, completion: nil)
     }
 }
+
+extension UserInfoViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.isEnabled = false
+    }
+}
+
+extension UserInfoViewController: UITextViewDelegate {
+    func textViewDidEndEditing(_ textView: UITextView) {
+        textView.resignFirstResponder()
+        textView.isEditable = false
+        textView.isSelectable = false
+    }
+}
+
